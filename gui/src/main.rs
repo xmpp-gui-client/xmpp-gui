7dcdd16b29fbf2060749540e70b042bd59be0b60
moh <mohamm.alsaid@gmail.com>
2021-06-08 03:35:18 -0700

    create simple jid & passwd interface

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,6 +1,6 @@
 mod connection_handler;
-use connection_handler::Handler as ConnHandler;
 use iced::{Result, Sandbox, Settings};
+use connection_handler::Handler as ConnHandler;
 
 fn main() -> Result {
     ConnHandler::run(Settings::default())
