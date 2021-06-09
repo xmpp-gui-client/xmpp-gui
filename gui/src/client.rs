5fcb3e1042ff6d12b55b468f8bff86ef1a881dd3
moh <mohamm.alsaid@gmail.com>
2021-06-09 01:47:37 -0700

    add join room logic

--- gui/src/client.rs
+++ gui/src/client.rs
@@ -1,20 +1,23 @@
 use iced::Color;
 use xmpp::{ClientBuilder, ClientFeature, ClientType, Event};
+// use ;
 
 #[derive(Default)]
 pub struct State {
     pub jid: String,
     pub passwd: String,
     pub status: String,
+    pub room: String,
+    pub room_status: String,
+    domain: String,
     features: Vec<String>,
 }
 
-
-impl State{
+impl State {
     // fn new() -> Self{
     //     Self::default()
     // }
-    pub fn color(&self) -> Color{
+    pub fn color(&self) -> Color {
         let max = 255.0;
         match &self.status.to_lowercase()[..] {
             "connected" => Color {
@@ -23,136 +26,149 @@ impl State{
                 b: 0.,
                 a: max,
             },
-            _ => Color {
+            "not connected" => Color {
                 r: max,
                 g: 0.,
                 b: 0.,
                 a: max,
             },
+            _ => Color::BLACK,
         }
     }
 
     /// validates the states jid
     fn valid_jid(&self) -> bool {
-        if self.jid.contains('@'){
-            let v:Vec<&str> = self.jid.split('@').collect();
-            if v.len() >1 && !v[0].is_empty() && !v[1].is_empty(){
+        if self.jid.contains('@') {
+            let v: Vec<&str> = self.jid.split('@').collect();
+            if v.len() > 1 && !v[0].is_empty() && !v[1].is_empty() {
                 return true;
             }
         }
         false
     }
-    
+
     #[tokio::main]
     async fn conn(&mut self) {
-        let mut client_builder = ClientBuilder::new(&self.jid,&self.passwd).set_client(ClientType::Pc, "xmpp-rs");
-        println!("features: {}",self.features.len());
-        for f in self.features.iter(){
+        let mut client_builder =
+            ClientBuilder::new(&self.jid, &self.passwd).set_client(ClientType::Pc, "xmpp-rs");
+        for f in self.features.iter() {
             client_builder = client_builder.enable_feature(self.get_feature(f));
         }
         let mut client = client_builder.build().unwrap();
-        while let Some(events) = client.wait_for_events().await{
-                for event in events{
-                    match event {
-                        Event::Online => {
-                            self.set_status("Connected");
-                            return;
-                        },
-                        Event::Disconnected => {
-                            self.set_status("Disconnected");
-                            return;
-                        },
-                        _ => {
-                            self.set_status("Failed");
-                            return;
-                        },
-                    }
+        if let Some(events) = client.wait_for_events().await {
+            match events[0] {
+                Event::Online => {
+                    self.set_status("Connected");
+                }
+                Event::Disconnected => {
+                    self.set_status("Not connected");
+                }
+                _ => {
+                    self.set_status("Failed");
                 }
             }
+        }
     }
-    pub fn connect(&mut self){
-        if !self.valid_jid(){
+    pub fn connect(&mut self) {
+        if !self.valid_jid() {
             self.status = "Invalid JID".to_string();
             return;
         }
-
-        // call async connect method
-        // let res = timeout(Duration::from_secs(1), self.conn()).await;
         self.conn();
-        // ();
     }
 
-    pub fn set_jid(&mut self,jid:&str){
+    // #[tokio::main]
+    fn join(&mut self) {
+        let mut client_builder =
+            ClientBuilder::new(&self.jid, &self.passwd).set_client(ClientType::Pc, "xmpp-rs");
+        for f in self.features.iter() {
+            client_builder = client_builder.enable_feature(self.get_feature(f));
+        }
+        self.room_status = "failed to connect".to_string();
+    }
+    pub fn join_room(&mut self) {
+        if !self.valid_jid() || self.status.to_lowercase() != "connected" {
+            self.room_status = "Not joined".to_string();
+            return;
+        }
+        let v: Vec<&str> = self.jid.split('@').collect();
+        if v.len() > 1 && !v[0].is_empty() && !v[1].is_empty() {
+            self.domain = v[1].to_string();
+        }
+        self.join();
+    }
+    pub fn set_jid(&mut self, jid: &str) {
         self.jid = jid.to_string();
     }
 
-    pub fn set_passwd(&mut self,pass:&str){
+    pub fn set_passwd(&mut self, pass: &str) {
         self.passwd = pass.to_string();
     }
-    pub fn set_status(&mut self,status:&str){
+    pub fn set_status(&mut self, status: &str) {
         self.status = status.to_string();
     }
 
-    fn add_feature(&mut self,feature:&str){
+    fn add_feature(&mut self, feature: &str) {
         let exists = self.feature_exist(feature);
-        if !exists{
+        if !exists {
             self.features.push(feature.to_lowercase());
         }
     }
-    fn remove_feature(&mut self,feature:&str){
+    fn remove_feature(&mut self, feature: &str) {
         let f = &feature.to_lowercase()[..];
-        self.features.retain(|x| x!=f);
+        self.features.retain(|x| x != f);
     }
-    pub fn feature_exist(&self,feature:&str)->bool{
+    pub fn feature_exist(&self, feature: &str) -> bool {
         let feature = &feature.to_lowercase()[..];
-        self.features.iter().any(|f| f==feature)
+        self.features.iter().any(|f| f == feature)
     }
 
-    fn get_feature(&self,f:&str)->ClientFeature{
-        match &f.to_lowercase()[..]{
+    pub fn set_room(&mut self, name: &str) {
+        self.room = name.to_string();
+    }
+
+    fn get_feature(&self, f: &str) -> ClientFeature {
+        match &f.to_lowercase()[..] {
             "joinrooms" => ClientFeature::JoinRooms,
             "avatars" => ClientFeature::Avatars,
             _ => ClientFeature::ContactList,
         }
     }
 
-    pub fn feature_toggle(&mut self,feature:&str)->bool{
-        if self.feature_exist(feature){
+    pub fn feature_toggle(&mut self, feature: &str) -> bool {
+        if self.feature_exist(feature) {
             self.remove_feature(feature);
             return false;
         }
-        self.add_feature(feature);        
+        self.add_feature(feature);
         true
     }
-
-
 }
 
-
 #[test]
-fn test_valid_jid(){
+fn test_valid_jid() {
     let mut state = State::default();
-    
+
     state.set_jid("test");
-    assert_eq!(state.valid_jid(),false);
+    assert_eq!(state.valid_jid(), false);
 
     state.set_jid("test@");
-    assert_eq!(state.valid_jid(),false);
+    assert_eq!(state.valid_jid(), false);
 
     state.set_jid("@test");
-    assert_eq!(state.valid_jid(),false);
+    assert_eq!(state.valid_jid(), false);
 
     state.set_jid("test@test");
-    assert_eq!(state.valid_jid(),true);
+    assert_eq!(state.valid_jid(), true);
 }
 #[test]
-fn test_connect_successful(){
+fn test_connect_successful() {
     // this test uses a known valid account on an xmpp server
     let mut state = State::default();
     state.set_jid("moe@jabber.de");
     state.set_passwd("1111");
     state.connect();
-    assert_eq!(state.status.to_lowercase(),"connected");
+    assert_eq!(state.status.to_lowercase(), "connected");
 }
 
 #[test]
@@ -160,7 +176,7 @@ fn test_connect_failure_invalid_jid() {
     let mut state = State::default();
     state.set_jid("test@");
     state.connect();
-    assert_eq!(state.status.to_lowercase(),"invalid jid");
+    assert_eq!(state.status.to_lowercase(), "invalid jid");
 }
 
 #[test]
@@ -168,11 +184,11 @@ fn test_connect_failure_disconnect() {
     let mut state = State::default();
     state.set_jid("non_existant@localhost"); // I own the cs510rust.com & have no server set up on it
     state.connect();
-    assert_eq!(state.status.to_lowercase(),"disconnected");
+    assert_eq!(state.status.to_lowercase(), "not connected");
 }
 
 #[test]
-fn test_add_feature(){
+fn test_add_feature() {
     let mut s = State::default();
     s.add_feature("JoinRooms");
     assert!(s.feature_exist("joinrooms"));
@@ -200,7 +216,6 @@ fn test_remove_feature() {
     assert!(s.feature_exist("JoinRooms"));
     assert!(s.feature_exist("ContactList"));
 
-
     assert!(s.feature_exist("ContactList"));
     s.remove_feature("ContactList");
     assert!(!s.feature_exist("ContactList"));
@@ -209,10 +224,6 @@ fn test_remove_feature() {
     s.remove_feature("JoinRooms");
     assert!(!s.feature_exist("JoinRooms"));
 
-
-
     s.remove_feature("Avatars");
     assert!(!s.feature_exist("Avatars"));
-
-
 }
--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,15 +1,15 @@
 // things components used from the iced crate
-use iced::{button, Sandbox,Button, Element, Row,Column, Text,TextInput,text_input,Checkbox,Settings};
+use iced::{
+    button, text_input, Button, Checkbox, Column, Element, Row, Sandbox, Settings, Text, TextInput,
+};
 // use xmpp_parsers::{message::MessageType, Jid};
 // use xmpp::ClientFeature;
 mod client;
 use client::State;
 
-
 fn main() -> iced::Result {
     App::run(Settings::default())
 }
-
 
 /// first component required by Iced (state)
 #[derive(Default)]
@@ -17,29 +17,31 @@ pub struct App {
     // connection_status: &'a str,
     connect_button: button::State, // used to indicate connection to server
     disconnect_button: button::State, // used to indicate disconnection from server
+    join_room_button: button::State,
 
     jid_input: text_input::State,
     passwd_input: text_input::State,
-    
+    room_name_input: text_input::State,
+
     avatars_feature: bool,
-    join_room_feature:bool,
+    join_room_feature: bool,
     contact_list_feature: bool,
     state: State,
 }
-
 
 /// second component required by Iced (interactions)
 #[derive(Debug, Clone)]
 pub enum GuiEvent {
     Connect,
     Disconnect,
+    JoinRoom,
     JidChanged(String),
     PasswdChanged(String),
     ToggleAvatar(bool),
     ToggleJoinRoom(bool),
     ToggleContactList(bool),
+    RoomNameChanged(String),
 }
-
 
 impl Sandbox for App {
     // type Executor = executor::Default;
@@ -57,89 +59,135 @@ impl Sandbox for App {
         Column::new() // first row
             .push(
                 Row::new()
-                .push(
-                    TextInput::new(&mut self.jid_input,"JID",&self.state.jid,GuiEvent::JidChanged).padding(10),
-                )
-                .push(
-                    TextInput::new(&mut self.passwd_input,"Password",&self.state.passwd,GuiEvent::PasswdChanged).padding(10),
-                )
-                .max_width(500)
+                    .push(
+                        TextInput::new(
+                            &mut self.jid_input,
+                            "JID",
+                            &self.state.jid,
+                            GuiEvent::JidChanged,
+                        )
+                        .padding(10),
+                    )
+                    .push(
+                        TextInput::new(
+                            &mut self.passwd_input,
+                            "Password",
+                            &self.state.passwd,
+                            GuiEvent::PasswdChanged,
+                        )
+                        .padding(10),
+                    )
+                    .max_width(500),
             )
             .push(
                 Row::new()
-                .push(
-                    Button::new(&mut self.connect_button, Text::new("Connect"))
-                    .on_press(GuiEvent::Connect), // send connect event when clicked
-                )
-                .push(
-                    Button::new(&mut self.disconnect_button, Text::new("Disconned"))
-                    .on_press(GuiEvent::Disconnect), // send disconnect event
-                )
-                .push(
-                    // display connection status
-                    Text::new(&self.state.status)
-                    .size(30)
-                    .color(self.state.color())
-                )
-                .spacing(10)
+                    .push(
+                        Button::new(&mut self.connect_button, Text::new("Connect"))
+                            .on_press(GuiEvent::Connect), // send connect event when clicked
+                    )
+                    .push(
+                        Button::new(&mut self.disconnect_button, Text::new("Disconned"))
+                            .on_press(GuiEvent::Disconnect), // send disconnect event
+                    )
+                    .push(
+                        // display connection status
+                        Text::new(&self.state.status)
+                            .size(30)
+                            .color(self.state.color()),
+                    )
+                    .spacing(10),
             )
+            .push(Text::new("Features:").size(30))
             .push(
-                Text::new("Features:").size(30)
+                Row::new()
+                    .push(
+                        Column::new()
+                            .push(
+                                Checkbox::new(
+                                    self.join_room_feature,
+                                    "JoinRoom".to_owned(),
+                                    GuiEvent::ToggleJoinRoom,
+                                )
+                                .size(30),
+                            )
+                            .push(
+                                Checkbox::new(
+                                    self.avatars_feature,
+                                    "Avatars".to_owned(),
+                                    GuiEvent::ToggleAvatar,
+                                )
+                                .size(30),
+                            )
+                            .push(
+                                Checkbox::new(
+                                    self.contact_list_feature,
+                                    "ContactList".to_owned(),
+                                    GuiEvent::ToggleContactList,
+                                )
+                                .size(30),
+                            ),
+                    )
+                    .push(
+                        Column::new()
+                            .push(
+                                TextInput::new(
+                                    &mut self.room_name_input,
+                                    "Room",
+                                    &self.state.room,
+                                    GuiEvent::RoomNameChanged,
+                                )
+                                .padding(10),
+                            )
+                            .max_width(300)
+                            .push(
+                                Row::new()
+                                    .push(
+                                        Button::new(
+                                            &mut self.join_room_button,
+                                            Text::new("Join room"),
+                                        )
+                                        .on_press(GuiEvent::JoinRoom),
+                                    )
+                                    .push(Text::new(&self.state.room_status).size(30))
+                                    .spacing(10),
+                            ),
+                    )
+                    .spacing(50),
             )
-            .push(
-                Checkbox::new(
-                    self.join_room_feature,
-                    "JoinRoom".to_owned(),
-                    GuiEvent::ToggleJoinRoom,
-                ).size(30)
-            )
-            .push(
-                Checkbox::new(
-                    self.avatars_feature,
-                    "Avatars".to_owned(),
-                    GuiEvent::ToggleAvatar,
-                ).size(30)
-            )
-            .push(
-                Checkbox::new(
-                    self.contact_list_feature,
-                    "ContactList".to_owned(),
-                    GuiEvent::ToggleContactList,
-                ).size(30)
-            )
+            .padding(100)
+            .spacing(20)
             .into()
     }
 
     /// fourth component needed (reaction to interactions -- udpate state)
-    fn update(&mut self, message: GuiEvent){
+    fn update(&mut self, message: GuiEvent) {
         match message {
             GuiEvent::Connect => {
-                // let jid = &self.state.jid;
-                // let passwd = &self.state.passwd;
-                // if self.state.valid_jid() {
-                //     self.state.status = "Invalid JID".to_string();
-                // }
+                self.state.set_status("");
                 self.state.connect();
-            },
+            }
             GuiEvent::Disconnect => {
                 self.state.status = "Disconnected".to_string();
-            },
+            }
             GuiEvent::JidChanged(s) => {
+                self.state.set_status("");
                 self.state.set_jid(&s);
-            },
+            }
             GuiEvent::PasswdChanged(s) => {
+                self.state.set_status("");
                 self.state.set_passwd(&s);
-            },
-            GuiEvent::ToggleContactList(_) =>{
+            }
+            GuiEvent::ToggleContactList(_) => {
                 self.contact_list_feature = self.state.feature_toggle("ContactList");
-            },
-            GuiEvent::ToggleAvatar(_) =>{
+            }
+            GuiEvent::ToggleAvatar(_) => {
                 self.avatars_feature = self.state.feature_toggle("avatars");
-            },
-            GuiEvent::ToggleJoinRoom(_) =>{
+            }
+            GuiEvent::ToggleJoinRoom(_) => {
                 self.join_room_feature = self.state.feature_toggle("joinRooms");
-            }  
-
+            }
+            GuiEvent::RoomNameChanged(name) => self.state.set_room(&name),
+            GuiEvent::JoinRoom => self.state.join_room(),
         }
     }
 }
